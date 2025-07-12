3a9c52c7b398f3bd004ce040c80c7cfe66421954
Kelv-alt <noel.kel.vincent@gmail.com>
2025-07-12 20:51:47 +0100

    userCard

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css';
 import React from 'react';
-import MainContent from "./components/MainContent";
-import Header from './components/Header';
-import Footer from './components/Footer';
-
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
     </div>
   );
 }
