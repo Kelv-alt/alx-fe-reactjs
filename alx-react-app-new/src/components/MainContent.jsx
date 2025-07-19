aade5c5595d33f9ee237b692f3901e4224190a87
Kelv-alt <noel.kel.vincent@gmail.com>
2025-07-20 00:42:10 +0100

    upgrade userProfile

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+function MainContent() {
+  return (
+    <main>
+      <p style={{ fontStyle: "italic" }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
