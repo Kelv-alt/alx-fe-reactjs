cc8006faf9affbd40df6cc6159e5eb0fe89c687d
Kelv-alt <noel.kel.vincent@gmail.com>
2025-07-20 00:48:43 +0100

    added a counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+      <Counter />
       <Footer />
     </div>
 
