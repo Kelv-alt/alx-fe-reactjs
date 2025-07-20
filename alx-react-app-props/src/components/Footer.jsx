0f1fd2f036a439f6e921b7d154c4f0905455f420
Kelv-alt <noel.kel.vincent@gmail.com>
2025-07-20 02:26:16 +0100

    userContext

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p><span style={{ fontWeight: "bold" , color: "navy" }}>© 2023</span> City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
