d81cea4d19c9c877f28ba9da10a762ca35fd1cc5
Kelv-alt <noel.kel.vincent@gmail.com>
2025-07-20 02:40:42 +0100

    user

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import { useContext } from 'react';
+import UserContext from '../UserContext'; // Adjust the path if needed
 
-const UserProfile = (props) => {
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
-      <h2 style={{ color: "blue" }}>{props.name}</h2>
-      <p>Age: <span style={{fontWeight: "bold"}}>{props.age}</span> </p>
-      <p>Bio: {props.bio}</p>
+    <div>
+      <h2>{userData.name}</h2>
+      <p>{userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
+
