1e7dbe6abd6840a7b4a1b3bd5c205bb1c85fdeb0
Kelv-alt <noel.kel.vincent@gmail.com>
2025-07-26 17:29:21 +0100

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
-// src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import RecipeDetail from './pages/RecipeDetail';
+import AddRecipeForm from './pages/AddRecipeForm';  // import the new form component
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/add-recipe" element={<AddRecipeForm />} />  {/* new route */}
       </Routes>
     </BrowserRouter>
   );
