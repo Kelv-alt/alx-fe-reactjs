e79d6c9b7d360814e6355aece2b3660908f7407f
Kelv-alt <noel.kel.vincent@gmail.com>
2025-08-22 01:37:51 +0100

    query

---
+++ react-query-demo/src/Components/PostsComponent.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const fetchPosts = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return data;
+};
+
+function PostsComponent() {
+  const {
+    data: posts,
+    isLoading,
+    isError,
+    error,
+    refetch,
+    isFetching,
+  } = useQuery('posts', fetchPosts, {
+    staleTime: 1000 * 60, // 1 minute
+    cacheTime: 1000 * 60 * 5, // 5 minutes
+  });
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()} disabled={isFetching}>
+        {isFetching ? 'Refreshing...' : 'Refetch Posts'}
+      </button>
+
+      <ul>
+        {posts.slice(0, 10).map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
