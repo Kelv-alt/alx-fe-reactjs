a258dc8bfa7edf4368610125bdfdc057e1a1f8ef
Kelv-alt <noel.kel.vincent@gmail.com>
2025-08-22 01:16:58 +0100

    form

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,13 @@
+import RegistrationForm from './components/RegistrationForm';
+import React, { useState } from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Controlled Registration Form</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
